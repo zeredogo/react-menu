f4459b0911f48fc9e8572b1a1ed0177ef737350f
zeredogo <zeredogo@gmail.com>
2022-07-04 09:01:23 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  return (
+   <main>
+    <section className='menu section'>
+      <div className='title'>
+        <h2>our menu</h2>
+        <div className='underline'></div>
+      </div>
+      <Categories />
+      <Menu items={items}/>
+    </section>
+   </main>
+  );
+}
+
+export default App;
---
+++ src/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Menu = ({items}) => {
+  return <div className='section-center'>
+    {items.map((menuItem) => {
+      const {id, title, img, desc} = menuItem;
+      return <article key={id} className="menu-item">
+        <img src={img} alt={title} className='photo'/>
+      </article>
+    })}
+  </div>;
+};
+
+export default Menu;
