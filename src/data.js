f99750c8d82a8cfe5cdc62e454e51a1ff7720184
zeredogo <zeredogo@gmail.com>
2022-07-04 14:13:51 -0700

    updating commit

--- src/data.js
+++ src/data.js
@@ -1,10 +1,22 @@
+import Item1 from './images/item-1.jpeg';
+import Item2 from './images/item-2.jpeg';
+import Item3 from './images/item-3.jpeg';
+import Item4 from './images/item-4.jpeg';
+import Item5 from './images/item-5.jpeg';
+import Item6 from './images/item-6.jpeg';
+import Item7 from './images/item-7.jpeg';
+import Item8 from './images/item-8.jpeg';
+import Item9 from './images/item-9.jpeg';
+import Item10 from './images/item-10.jpeg';
+import Item11 from './images/item-11.jpeg';
+import Item12 from './images/item-12.jpeg';
 const menu = [
   {
     id: 1,
     title: 'buttermilk pancakes',
     category: 'breakfast',
     price: 15.99,
-    img: './images/item-1.jpeg',
+    img: `${Item1}`,
     desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
   },
   {
@@ -12,15 +24,15 @@ const menu = [
     title: 'diner double',
     category: 'lunch',
     price: 13.99,
-    img: './images/item-2.jpeg',
+    img: `${Item2}`,
     desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
   },
   {
     id: 3,
-    title: 'godzilla milkshake',
-    category: 'shakes',
+    title: 'godzilla ',
+    category: 'lunch',
     price: 6.99,
-    img: './images/item-3.jpeg',
+    img: `${Item3}`,
     desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
   },
   {
@@ -28,7 +40,7 @@ const menu = [
     title: 'country delight',
     category: 'breakfast',
     price: 20.99,
-    img: './images/item-4.jpeg',
+    img: `${Item4}`,
     desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
   },
   {
@@ -36,7 +48,7 @@ const menu = [
     title: 'egg attack',
     category: 'lunch',
     price: 22.99,
-    img: './images/item-5.jpeg',
+    img: `${Item5}`,
     desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
   },
   {
@@ -44,7 +56,7 @@ const menu = [
     title: 'oreo dream',
     category: 'shakes',
     price: 18.99,
-    img: './images/item-6.jpeg',
+    img: `${Item6}`,
     desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
   },
   {
@@ -52,7 +64,7 @@ const menu = [
     title: 'bacon overflow',
     category: 'breakfast',
     price: 8.99,
-    img: './images/item-7.jpeg',
+    img: `${Item7}`,
     desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
   },
   {
@@ -60,15 +72,39 @@ const menu = [
     title: 'american classic',
     category: 'lunch',
     price: 12.99,
-    img: './images/item-8.jpeg',
+    img: `${Item8}`,
     desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
   },
   {
     id: 9,
-    title: 'quarantine buddy',
+    title: 'thurnder',
     category: 'shakes',
     price: 16.99,
-    img: './images/item-9.jpeg',
+    img: `${Item12}`,
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 10,
+    title: 'chicken rice',
+    category: 'lunch',
+    price: 16.99,
+    img: `${Item10}`,
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 11,
+    title: 'chicken',
+    category: 'lunch',
+    price: 16.99,
+    img: `${Item9}`,
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 12,
+    title: 'buddy',
+    category: 'shakes',
+    price: 16.99,
+    img: `${Item11}`,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
